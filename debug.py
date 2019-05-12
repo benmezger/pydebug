c016a1ee56d8627301a06bdca3118a75995e23b4
Ben Mezger <me@benmezger.nl>
2019-05-12 17:21:41 -0300

    Allow PDB to be set on error

--- debug.py
+++ debug.py
@@ -9,6 +9,7 @@ import pstats
 import os
 import errno
 import warnings
+import traceback
 
 try:
     from django.conf import settings as dsettings
@@ -19,9 +20,13 @@ except ImportError:
 
 try:
     from ipdb import runcall
+    from ipdb import launch_ipdb_on_exception
+    has_ipdb = True
 except ImportError:
     warnings.warn("Unable to import IPDB. Using PDB instead", ImportWarning)
     from pdb import runcall
+    from pdb import Pdb
+    has_ipdb = False
 
 try:
     from line_profiler import LineProfiler
@@ -70,7 +75,6 @@ class DebugDecorator:
 
         # we ignore if it's a subclass
         if self.is_subcls:
-            print(self.func)
             return self.func(*args, **kwargs)
 
         # Ignore if it's a Django project and debug is set to False
@@ -136,7 +140,8 @@ class ObjectInfoDebug(DebugDecorator):
 
 
 class PDBDebugger(DebugDecorator):
-    def __init__(self, logger=None):
+    def __init__(self, logger=None, on_error=False):
+        self.on_error = on_error
         super().__init__(logger)
 
     def setup(self, *args, **kwargs):
@@ -146,8 +151,17 @@ class PDBDebugger(DebugDecorator):
         pass
 
     def debug_func(self, *args, **kwargs):
-        return runcall(self.func, *args, **kwargs)
-
+        if not self.on_error:
+            return runcall(self.func, *args, **kwargs)
+        try:
+            if has_ipdb:
+                with launch_ipdb_on_exception():
+                    return self.func(*args, **kwargs)
+            else:
+                return self.func(*args, **kwargs)
+        except Exception as e:
+            traceback.print_exc(file=sys.stderr)
+            Pdb(stdin=sys.__stdin__, stdout=sys.__stdout__).set_trace()
 
 class DisassembleDebug(DebugDecorator):
     def __init__(self, logger=None):
