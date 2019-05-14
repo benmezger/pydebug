7be657fe3eacc881e54afeeb56c0ddc56d961990
Ben Mezger <me@benmezger.nl>
2019-05-14 16:13:58 -0300

    Fix: Add list of requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+requires = ["Cython>=0.29.7", "ipdb>=0.12"]
+
 setup(
     name="python-pydebug",
     version="0.2",
@@ -14,5 +16,8 @@ setup(
     author_email="me@benmezger.nl",
     license="MIT",
     packages=["pydebug"],
+    install_requires=requires,
     zip_safe=False,
+    python_requires=">=3.*.*",
+    extras_require={"line_profiling": ["line_profiler>=2.1.1"]},
 )
